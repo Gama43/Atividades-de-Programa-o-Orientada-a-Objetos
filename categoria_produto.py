7df25a23dbd054d3c97dabf87dcf7c9150f37172
Gabriel Rodrigo da Gama <61113905+Gama43@users.noreply.github.com>
2022-05-13 07:43:09 -0300

    Update and rename categoria_produto.py to atividade_2_categoria_produto.py

--- categoria_produto.py
+++
@@ -1,9 +0,0 @@
-class CategoriaProduto:
-    def __init__(self, titulo):
-        self.__titulo = titulo
-
-    def setTitulo(self,titulo):
-        self.__titulo = titulo
-
-    def getTitulo(self):
-      return self.__titulo
