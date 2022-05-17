a323365c0182cdfe8a05ee9d94b795e348be70a8
Gabriel Rodrigo da Gama <61113905+Gama43@users.noreply.github.com>
2022-05-17 07:46:44 -0300

    Update atividade_4_Imovel.py

--- atividade_4_Imovel.py
+++ atividade_4_Imovel.py
@@ -2,86 +2,90 @@ from Locatario import Locatario
 from Locador import Locador
 from Mobilia import Mobilia
 
+
 class Imovel:
-    def __init__(self, codigo: int, descricao: str, valor: float, locador: Locador):
+    def __init__(self, codigo: int, descricao: str,
+                 valor: float, locador: Locador):
         self.__codigo = codigo
         self.__descricao = descricao
         self.__valor = valor
         self.__locador = locador
         self.__locatarios = []
         self.__mobilias = []
-        
+
     @property
     def codigo(self):
-      if isinstance(self.__codigo, int): 
-        return self.__codigo
+        if isinstance(self.__codigo, int):
+            return self.__codigo
 
     @codigo.setter
     def codigo(self, codigo):
-      if isinstance(self.__codigo, int): 
-        self.__codigo = codigo
+        if isinstance(self.__codigo, int):
+            self.__codigo = codigo
 
     @property
     def descricao(self):
-      if isinstance(self.__descricao, str): 
-        return self.__descricao
+        if isinstance(self.__descricao, str):
+            return self.__descricao
 
     @descricao.setter
     def descricao(self, descricao):
-      if isinstance(self.__descricao, str): 
-        self.__descricao = descricao 
+        if isinstance(self.__descricao, str):
+            self.__descricao = descricao
 
     @property
     def valor(self):
-      if isinstance(self.__valor, float): 
-        return self.__valor
+        if isinstance(self.__valor, float):
+            return self.__valor
 
     @valor.setter
     def valor(self, valor):
-      if isinstance(self.__valor, float): 
-        self.__valor = valor 
+        if isinstance(self.__valor, float):
+            self.__valor = valor
 
     @property
     def locador(self):
-      if isinstance(self.__locador, Locador): 
-        return self.__locador
+        if isinstance(self.__locador, Locador):
+            return self.__locador
 
     @locador.setter
     def locador(self, locador):
-      if isinstance(self.__locador, Locador): 
-        self.__locador = locador
+        if isinstance(self.__locador, Locador):
+            self.__locador = locador
 
+    @property
     def locatarios(self):
-      return self.__locatarios
-  
-    def incluir_locatario(self, locatario: Locatario):
-      if isinstance(locatario, Locatario) and locatario not in self.__locatarios:
-        self.__locatarios.append(locatario)
-      
-        # Nao esqueca de garantir que o objeto recebido pertence a classe Locatario...
-        # Nao permitir insercao de Locatarios duplicados!
-        
-    def excluir_locatario(self, codigo_locatario: int):
-        if isinstance(locatario, Locatario) and locatario in self.__locatarios:
-          self.__locatarios.remove(locatario)
+        return self.__locatarios
 
+    def incluir_locatario(self, locatario: Locatario):
+        if isinstance(locatario, Locatario) and (locatario not in
+                                                 self.__locatarios):
+            self.__locatarios.append(locatario)
+
+    def excluir_locatario(self, codigo_locatario: int):
+        for locatario in self.__locatarios:
+            if locatario.codigo == codigo_locatario:
+                self.__locatarios.remove(locatario)
+
+    @property
     def mobilias(self):
-      return self.__mobilias
+        return self.__mobilias
 
     def incluir_mobilia(self, codigo_mobilia: int, descricao_mobilia: str):
-        if isinstance(mobilias, Mobilia):
-          self.__mobilias.append(Mobilia(codigo, descricao))
+        situacao = True
+        for mobilia in self.__mobilias:
+            if mobilia.codigo == codigo_mobilia:
+                situacao = False
+                break
+        if situacao is True:
+            self.__mobilias.append(self)
 
     def excluir_mobilia(self, codigo_mobilia: int):
-        if isinstance(mobilias, Mobilia):
-          self.__mobilias.remove(Mobilia(codigo, descricao))
-        
+        for mobilia in self.__mobilias:
+            if mobilia.codigo == codigo_mobilia and mobilia in self.__mobilias:
+                self.__mobilias.remove(mobilia)
 
     def find_locatario_by_codigo(self, codigo_locatario: int):
         for locatario in self.__locatarios:
-          if locatario.codigo == codigo_locatario:
-              return locatario
-
-
-        # Procura na lista de locatarios se existe um Locatario com este codigo 
-        # Se encontrar, retorna o Locatario encontrado
+            if locatario.codigo == codigo_locatario:
+                return locatario
