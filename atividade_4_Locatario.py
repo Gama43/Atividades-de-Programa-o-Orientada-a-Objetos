2d331f34f10c6595e1703b85ddae13de0c4b453d
Gabriel Rodrigo da Gama <61113905+Gama43@users.noreply.github.com>
2022-05-17 07:49:43 -0300

    Update atividade_4_Locatario.py

--- atividade_4_Locatario.py
+++ atividade_4_Locatario.py
@@ -6,30 +6,30 @@ class Locatario:
 
     @property
     def codigo(self):
-        if isinstance(self.__codigo, int): 
-          return self.__codigo
+        if isinstance(self.__codigo, int):
+            return self.__codigo
 
     @codigo.setter
     def codigo(self, codigo):
-      if isinstance(self.__codigo, int): 
-        self.__codigo = codigo
+        if isinstance(codigo, int):
+            self.__codigo = codigo
 
     @property
     def nome(self):
-      if isinstance(self.__nome, str): 
-        return self.__nome
+        if isinstance(self.__nome, str):
+            return self.__nome
 
     @nome.setter
     def nome(self, nome):
-      if isinstance(self.__nome, str): 
-        self.__nome = nome
+        if isinstance(nome, str):
+            self.__nome = nome
 
     @property
     def telefone(self):
-      if isinstance(self.__telefone, int): 
-        return self.__telefone
+        if isinstance(self.__telefone, int):
+            return self.__telefone
 
     @telefone.setter
     def telefone(self, telefone):
-      if isinstance(self.__telefone, int): 
-        self.__telefone = telefone
+        if isinstance(telefone, int):
+            self.__telefone = telefone
