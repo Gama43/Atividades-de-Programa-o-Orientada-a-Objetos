e3dc1ff300d6e5e9ae35fbf00d0bb959ae88d688
Gabriel Rodrigo da Gama <61113905+Gama43@users.noreply.github.com>
2022-05-17 07:51:17 -0300

    Update atividade_4_Mobilia.py

--- atividade_4_Mobilia.py
+++ atividade_4_Mobilia.py
@@ -5,21 +5,18 @@ class Mobilia:
 
     @property
     def codigo(self):
-      if isinstance(self.__codigo, int): 
         return self.__codigo
 
     @codigo.setter
     def codigo(self, codigo):
-      if isinstance(self.__codigo, int): 
-        self.__codigo = codigo
+        if isinstance(codigo, int):
+            self.__codigo = codigo
 
     @property
     def descricao(self):
-      if isinstance(self.__descricao, str): 
         return self.__descricao
 
     @descricao.setter
     def descricao(self, descricao):
-      if isinstance(self.__descricao, str): 
-        self.__descricao = descricao  
-       
+        if isinstance(descricao, str):
+            self.__descricao = descricao
